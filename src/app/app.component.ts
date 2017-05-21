6c5c26a68723e0e8eb677ae451853b6f54f84d5d
Ajaykumar <pajay2507@gmail.com>
2017-05-21 15:08:48 -0400

    Added media item components and templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'media-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
